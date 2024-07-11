1475ba1a5feb7ee5ba6ca5829e3278e071cb66f2
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-12 07:53:11 +0900

    valitateついか

--- api/v1/bwh/bwh.go
+++ api/v1/bwh/bwh.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	validation "github.com/go-ozzo/ozzo-validation"
 )
 
 type BWH struct {
@@ -20,12 +21,33 @@ type BWH struct {
 	Weight  *int64 `db:"weight" json:"weight"`
 }
 
+func (b *BWH) Validate() error {
+	return validation.ValidateStruct(b,
+		validation.Field(&b.EntryID, validation.Required),
+		validation.Field(&b.Bust, validation.Required),
+		validation.Field(&b.Waist, validation.Required),
+		validation.Field(&b.Hip, validation.Required),
+	)
+}
+
 type BWHsJson struct {
 	BWHs []BWH `json:"bwhs"`
 }
 
+func (b *BWHsJson) Validate() error {
+	return validation.ValidateStruct(b,
+		validation.Field(&b.BWHs, validation.Required),
+	)
+}
+
 type IDs struct {
 	IDs []int64 `json:"ids"`
+}
+
+func (i *IDs) Validate() error {
+	return validation.ValidateStruct(i,
+		validation.Field(&i.IDs, validation.Required),
+	)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
