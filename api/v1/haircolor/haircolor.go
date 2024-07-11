29685518f5c24869568c89032d8b19295eea54a6
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-12 08:11:54 +0900

    create

---
+++ api/v1/haircolor/haircolor.go
@@ -0,0 +1,101 @@
+package haircolor
+
+import (
+	"encoding/json"
+	"log"
+	"io"
+	"fmt"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type HairColor struct {
+	EntryID int64 `db:"entry_id"`
+	ColorID int64 `db:"color_id"`
+}
+
+func (h *HairColor) Validate() error {
+	return validation.ValidateStruct(h,
+		validation.Field(&h.EntryID, validation.Required),
+		validation.Field(&h.ColorID, validation.Required),
+	)
+}
+
+type HairColorsJson struct {
+	HairColors []HairColor `json:"haircolors"`
+}
+
+func (h *HairColorsJson) Validate() error {
+	return validation.ValidateStruct(h,
+		validation.Field(&h.HairColors, validation.Required),
+	)
+}
+
+type IDs struct {
+	IDs []int64 `json:"ids"`
+}
+
+func (i *IDs) Validate() error {
+	return validation.ValidateStruct(i,
+		validation.Field(&i.IDs, validation.Required),
+	)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	db, err := sqlx.Open("postgres", "")
+	if err != nil {
+		log.Printf("sql.Open error %s", err)
+	}
+	defer db.Close()
+	switch r.Method {
+	case http.MethodGet:
+	case http.MethodPost:
+		var hairColorsJson HairColorsJson
+		query := `
+			INSERT INTO haircolor (
+				entry_id,
+				color_id
+			) VALUES (
+				:entry_id,
+				:color_id
+			)
+		`
+		// json読み込み
+		err := json.NewDecoder(r.Body).Decode(&hairColorsJson)
+		if err != nil {
+			log.Printf(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		// jsonバリデーション
+		err = hairColorsJson.Validate()
+		if err != nil {
+			log.Printf(fmt.Sprintf("validation error: %v", err))
+			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		}
+		for _, hc := range hairColorsJson.HairColors {
+			// jsonバリデーション
+			err = hc.Validate()
+			if err != nil {
+				log.Printf(fmt.Sprintf("validation error: %v", err))
+				http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+			}
+			if _, err := db.NamedExecContext(r.Context(), query, hc); err != nil {
+				log.Printf(fmt.Sprintf("db error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+		// json書き込み
+		err = json.NewEncoder(w).Encode(&hairColorsJson)
+		if err != nil {
+			log.Printf(fmt.Sprintf("json encode error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	case http.MethodPut:
+	case http.MethodDelete:
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
