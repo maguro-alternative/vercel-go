f6182d6186d52fc227dbb760932c9b50a8c5411f
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-12 07:39:05 +0900

    delete

--- api/v1/entry/entry.go
+++ api/v1/entry/entry.go
@@ -220,6 +220,67 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	case http.MethodDelete:
+		var delIDs IDs
+		query := `
+			DELETE FROM
+				entry
+			WHERE
+				id IN (?)
+		`
+		// リクエストボディを読み込む
+		jsonBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(fmt.Sprintf("read error: %v", err))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		err = json.Unmarshal(jsonBytes, &delIDs)
+		if err != nil {
+			log.Println(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		if len(delIDs.IDs) == 0 {
+			return
+		// 1件の場合はIN句を使わない
+		} else if len(delIDs.IDs) == 1 {
+			query = `
+				DELETE FROM
+					entry
+				WHERE
+					id = $1
+			`
+			// 1件削除
+			_, err = db.ExecContext(r.Context(), query, delIDs.IDs[0])
+			if err != nil {
+				log.Println(fmt.Sprintf("delete error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			// レスポンスボディに書き込む
+			err = json.NewEncoder(w).Encode(&delIDs)
+			if err != nil {
+				log.Println(fmt.Sprintf("json encode error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+		// idの数だけ置換文字を作成
+		query, args, err := sqlx.In(query, delIDs.IDs)
+		if err != nil {
+			log.Println(fmt.Sprintf("db.In error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// Postgresの場合は置換文字を$1, $2, ...とする必要がある
+		query = sqlx.Rebind(len(delIDs.IDs), query)
+		_, err = db.ExecContext(r.Context(), query, args...)
+		if err != nil {
+			log.Println(fmt.Sprintf("delete error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// レスポンスボディに書き込む
+		err = json.NewEncoder(w).Encode(&delIDs)
+		if err != nil {
+			log.Println(fmt.Sprintf("json encode error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
