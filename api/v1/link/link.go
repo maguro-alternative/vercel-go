5a6ebf9c72ecf66427542892b444b4e3a789a6c4
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-12 08:43:32 +0900

    create

---
+++ api/v1/link/link.go
@@ -0,0 +1,119 @@
+package link
+
+import (
+	"encoding/json"
+	"log"
+	"fmt"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type Link struct {
+	ID       int64  `db:"id"`
+	EntryID  int64  `db:"entry_id"`
+	Type     string `db:"type"`
+	URL      string `db:"url"`
+	Nsfw     bool   `db:"nsfw"`
+	Darkness bool   `db:"darkness"`
+}
+
+func (l *Link) Validate() error {
+	return validation.ValidateStruct(l,
+		validation.Field(&l.EntryID, validation.Required),
+		validation.Field(&l.Type, validation.Required),
+		validation.Field(&l.URL, validation.Required),
+		validation.Field(&l.Nsfw, validation.In(false, true)),
+		validation.Field(&l.Darkness, validation.In(false, true)),
+	)
+}
+
+type LinksJson struct {
+	Links []Link `json:"links"`
+}
+
+func (l *LinksJson) Validate() error {
+	return validation.ValidateStruct(l,
+		validation.Field(&l.Links, validation.Required),
+	)
+}
+
+type IDs struct {
+	IDs []int64 `json:"ids"`
+}
+
+func (i *IDs) Validate() error {
+	return validation.ValidateStruct(i,
+		validation.Field(&i.IDs, validation.Required),
+	)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	db, err := sqlx.Open("postgres", "")
+	if err != nil {
+		log.Printf("sql.Open error %s", err)
+	}
+	defer db.Close()
+	switch r.Method {
+	case http.MethodGet:
+	case http.MethodPost:
+		var linksJson LinksJson
+		query := `
+			INSERT INTO link (
+				entry_id,
+				type,
+				url,
+				nsfw,
+				darkness
+			) VALUES (
+				:entry_id,
+				:type,
+				:url,
+				:nsfw,
+				:darkness
+			)
+		`
+		// リクエストボディをJSONに変換
+		err := json.NewDecoder(r.Body).Decode(&linksJson)
+		if err != nil {
+			log.Printf(fmt.Sprintf("json decode error: %v body:%v", err, r.Body))
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// JSONのバリデーション
+		err = linksJson.Validate()
+		if err != nil {
+			log.Printf(fmt.Sprintf("validation error: %v", err))
+			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+			return
+		}
+		for _, link := range linksJson.Links {
+			// リンクのバリデーション
+			err = link.Validate()
+			if err != nil {
+				log.Printf(fmt.Sprintf("validation error: %v", err))
+				http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+				return
+			}
+			_, err = db.NamedExecContext(r.Context(), query, link)
+			if err != nil {
+				log.Printf(fmt.Sprintf("db error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+		// レスポンスボディをJSONに変換
+		err = json.NewEncoder(w).Encode(&linksJson)
+		if err != nil {
+			log.Printf(fmt.Sprintf("json encode error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPut:
+	case http.MethodDelete:
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
