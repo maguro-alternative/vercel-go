91bfadc82157f4826b958705a0f4eeddde39beec
ygo <ka.ra.si.naporitan@gmail.com>
2024-07-12 08:36:56 +0900

    get

--- api/v1/hairstyle_type/hairstyle_type.go
+++ api/v1/hairstyle_type/hairstyle_type.go
@@ -50,6 +50,86 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 	switch r.Method {
 	case http.MethodGet:
+		var hairStyleTypesJson HairStyleTypesJson
+		query := `
+			SELECT
+				id,
+				style
+			FROM
+				hairstyle_type
+			WHERE
+				id IN (?)
+		`
+		queryIDs, ok := r.URL.Query()["id"]
+		if !ok {
+			query = `
+				SELECT
+					id,
+					style
+				FROM
+					hairstyle_type
+			`
+			err := db.SelectContext(r.Context(), &hairStyleTypesJson.HairStyleTypes, query)
+			if err != nil {
+				log.Printf(fmt.Sprintf("db error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			// レスポンスの作成
+			err = json.NewEncoder(w).Encode(&hairStyleTypesJson)
+			if err != nil {
+				log.Printf(fmt.Sprintf("json encode error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		} else if len(queryIDs) == 1 {
+			query = `
+				SELECT
+					id,
+					style
+				FROM
+					hairstyle_type
+				WHERE
+					id = $1
+			`
+			err := db.SelectContext(r.Context(), &hairStyleTypesJson.HairStyleTypes, query, queryIDs[0])
+			if err != nil {
+				log.Printf(fmt.Sprintf("db error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			// レスポンスの作成
+			err = json.NewEncoder(w).Encode(&hairStyleTypesJson)
+			if err != nil {
+				log.Printf(fmt.Sprintf("json encode error: %v", err))
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+		// idの数だけ置換文字を作成
+		query, args, err := sqlx.In(query, queryIDs)
+		if err != nil {
+			log.Printf(fmt.Sprintf("db error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Postgresの場合は置換文字を$1, $2, ...とする必要がある
+		query = sqlx.Rebind(len(queryIDs), query)
+		err = db.SelectContext(r.Context(), &hairStyleTypesJson.HairStyleTypes, query, args...)
+		if err != nil {
+			log.Printf(fmt.Sprintf("db error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// レスポンスの作成
+		err = json.NewEncoder(w).Encode(&hairStyleTypesJson)
+		if err != nil {
+			log.Printf(fmt.Sprintf("json encode error: %v", err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	case http.MethodPost:
 		var hairStyleTypesJson HairStyleTypesJson
 		query := `
